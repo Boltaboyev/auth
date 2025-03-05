0dc520475f608dbe0c5d714515a128e82aa5ea95
Otabekcdr <boltaboyevotabek207@gmail.com>
2025-03-05 16:53:00 +0500

    auth

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import {StrictMode} from "react"
 import {createRoot} from "react-dom/client"
 import {RouterProvider} from "react-router-dom"
 import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
-import {ToastContainer} from "react-toastify"
+import {Bounce, ToastContainer} from "react-toastify"
 
 import {router} from "./router"
 
@@ -14,7 +14,20 @@ createRoot(document.getElementById("root")).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
             <RouterProvider router={router}></RouterProvider>
-            <ToastContainer />
+            <ToastContainer
+                position="top-right"
+                autoClose={1000}
+                limit={2}
+                hideProgressBar
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss={false}
+                draggable
+                pauseOnHover={false}
+                theme="colored"
+                transition={Bounce}
+            />
         </QueryClientProvider>
     </StrictMode>
 )
